d8709d42d55e0a2f636dae68497d3feaed882e82
flextrawurst <dakthinkpad@web.de>
2026-07-25 00:36:44 +0200

    action-history: Integrationskörper öffentlich exportieren

--- tools/action_history/__init__.py
+++ tools/action_history/__init__.py
@@ -1,4 +1,13 @@
 from .history import ActionHistory, HistoryIntegrityError
+from .lifecycle import startup_session
+from .toolset import REQUIRED_TOOLS, register_action_history_tools
 from .tracked_file_ops import TrackedFileOps
 
-__all__ = ["ActionHistory", "HistoryIntegrityError", "TrackedFileOps"]
+__all__ = [
+    "ActionHistory",
+    "HistoryIntegrityError",
+    "TrackedFileOps",
+    "REQUIRED_TOOLS",
+    "register_action_history_tools",
+    "startup_session",
+]
