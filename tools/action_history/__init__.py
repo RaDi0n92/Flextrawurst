5c747064e51f3e14001e2fee07834ab825d116a0
flextrawurst <dakthinkpad@web.de>
2026-07-25 01:05:54 +0200

    action-history: vollständigen Integrationsvertrag exportieren

--- tools/action_history/__init__.py
+++ tools/action_history/__init__.py
@@ -1,6 +1,13 @@
 from .decorators import tracked_mcp_action
 from .history import ActionHistory, HistoryIntegrityError
-from .lifecycle import begin_session_once, end_session_once, startup_session
+from .lifecycle import (
+    SessionClosedError,
+    begin_session_once,
+    end_session_once,
+    generate_session_id,
+    session_action,
+    startup_session,
+)
 from .reporting import build_session_report
 from .toolset import REQUIRED_TOOLS, register_action_history_tools
 from .tracked_file_ops import TrackedFileOps
@@ -8,12 +15,15 @@ from .tracked_file_ops import TrackedFileOps
 __all__ = [
     "ActionHistory",
     "HistoryIntegrityError",
+    "SessionClosedError",
     "TrackedFileOps",
     "REQUIRED_TOOLS",
     "begin_session_once",
     "build_session_report",
     "end_session_once",
+    "generate_session_id",
     "register_action_history_tools",
+    "session_action",
     "startup_session",
     "tracked_mcp_action",
 ]
