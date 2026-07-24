2c6afb54fd1eb36402e83db87f3a7790c66f9d53
flextrawurst <dakthinkpad@web.de>
2026-07-25 00:43:30 +0200

    action-history: Lebenszyklus und Berichtskörper exportieren

--- tools/action_history/__init__.py
+++ tools/action_history/__init__.py
@@ -1,6 +1,7 @@
 from .decorators import tracked_mcp_action
 from .history import ActionHistory, HistoryIntegrityError
-from .lifecycle import startup_session
+from .lifecycle import begin_session_once, end_session_once, startup_session
+from .reporting import build_session_report
 from .toolset import REQUIRED_TOOLS, register_action_history_tools
 from .tracked_file_ops import TrackedFileOps
 
@@ -9,6 +10,9 @@ __all__ = [
     "HistoryIntegrityError",
     "TrackedFileOps",
     "REQUIRED_TOOLS",
+    "begin_session_once",
+    "build_session_report",
+    "end_session_once",
     "register_action_history_tools",
     "startup_session",
     "tracked_mcp_action",
